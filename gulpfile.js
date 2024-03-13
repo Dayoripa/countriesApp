931dc6a8679dfd06b1ab986b6b1aa6b8cb26bdda
Daulin Riascos <daulinri@gmail.com>
2024-03-12 23:49:57 -0400

    Dependences sourcemaps and cssnano were installs

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,25 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-
-
-// imagenes
-// const imagemin = require('gulp-imagemin');
-
-
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 function css(done) {
     src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )
         .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css'))
 
     done()
 }
-
-// function images(  ) {
-//     return src('src/image/**/*')
-//         .pipe( imagemin({ optimizationLevel: 3 }) )
-//         .pipe( dest('build/img') );
-//  }
  
-
 function dev() {
     watch( 'src/scss/**/*.scss', css );
-    // watch( 'src/image/**/*', images);
 }
 
 
 exports.css = css;
 exports.dev = dev;
-// exports.images = images;
 exports.default = series( css, dev );
